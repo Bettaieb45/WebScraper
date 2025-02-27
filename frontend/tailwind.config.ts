399fe75d96d46d90b012be727d95ca56dc29abd6
aziz <mohamedazizbettaieb6@edu.bme.hu>
2025-02-27 21:48:48 +0100

    fixed layout.tsx

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -12,6 +12,15 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.8s ease-in-out forwards",
+      },
     },
   },
   plugins: [],
